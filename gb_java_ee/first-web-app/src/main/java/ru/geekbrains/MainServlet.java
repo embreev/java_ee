cd9c9a6ae2ae36ff202fe59273105f57c7f72b90
BreevE <e.m.breev@gmail.com>
2020-09-13 22:00:35 +0300

    added menu

--- gb_java_ee/first-web-app/src/main/java/ru/geekbrains/MainServlet.java
+++ gb_java_ee/first-web-app/src/main/java/ru/geekbrains/MainServlet.java
@@ -20,12 +20,12 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("New GET request to MainServlet");
 
-        resp.getWriter().println("<h1>Main</h1>");
-        resp.getWriter().println("<p>ПРИВЕТ</p>");
-        resp.getWriter().println("<p> contextPath = " + req.getContextPath() + "</p>");
-        resp.getWriter().println("<p> servletPath = " + req.getServletPath() + "</p>");
-        resp.getWriter().println("<p> pathInfo = " + req.getPathInfo() + "</p>");
-        resp.getWriter().println("<p> queryString = " + req.getQueryString() + "</p>");
+//        resp.getWriter().println("<h1>Main</h1>");
+//        resp.getWriter().println("<p>ПРИВЕТ</p>");
+//        resp.getWriter().println("<p> contextPath = " + req.getContextPath() + "</p>");
+//        resp.getWriter().println("<p> servletPath = " + req.getServletPath() + "</p>");
+//        resp.getWriter().println("<p> pathInfo = " + req.getPathInfo() + "</p>");
+//        resp.getWriter().println("<p> queryString = " + req.getQueryString() + "</p>");
 
     }
 }
